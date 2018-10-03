1fd6ca8bad5393241bdb2f923d51c48c8e29217c
ideasiii <louisju@iii.org.tw>
2018-10-03 20:35:58 +0800

    jugo_update

---
+++ app/src/main/java/org/iii/wheelpiedemo/MainActivity.java
@@ -0,0 +1,22 @@
+package org.iii.wheelpiedemo;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+    }
+}

70936411879b81c2c1602874f0660b065fef7b8a
ideasiii <louisju@iii.org.tw>
2018-10-05 11:47:34 +0800

    jugo_update

--- app/src/main/java/org/iii/wheelpiedemo/sample/VideoActivity.java
+++ app/src/main/java/org/iii/wheelpiedemo/sample/VideoActivity.java
@@ -1,25 +1,47 @@
 package org.iii.wheelpiedemo.sample;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
 
 import org.iii.wheelpiedemo.R;
 
-public class VideoActivity extends Activity
-{
-    RelativeLayout rlMain = null;
-    private static VideoPlayer videoPlayer = null;
+public class VideoActivity extends Activity {
+    private VideoPlayer videoPlayer1 = null;
+    private VideoPlayer videoPlayer2 = null;
+    private VideoPlayer videoPlayer3 = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-        rlMain = findViewById(R.id.rlVideoMain);
 
-        videoPlayer = new VideoPlayer(this);
-        videoPlayer.showController(true);
-        rlMain.addView(videoPlayer);
+        // 播放video stream
+        videoPlayer1 = findViewById(R.id.videoPlayer1);
+        videoPlayer1.showController(true);
+        Uri vidUri = Uri.parse("http://175.98.119.122/video/horisoz.mp4");
+        videoPlayer1.setVideo(vidUri);
+
+
+        videoPlayer2 = findViewById(R.id.videoPlayer2);
+        videoPlayer2.showController(true);
+        videoPlayer2.setVideo("android.resource://" + getPackageName() + "/" + R.raw.skidrow);
+
+        // videoPlayer3 不顯示控制條
+        videoPlayer3 = findViewById(R.id.videoPlayer3);
+        videoPlayer3.setVideo("android.resource://" + getPackageName() + "/" + R.raw.dio);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        videoPlayer1.play();
+    }
+
+    @Override
+    protected void onStop() {
+        videoPlayer1.stop();
+        super.onStop();
     }
 }

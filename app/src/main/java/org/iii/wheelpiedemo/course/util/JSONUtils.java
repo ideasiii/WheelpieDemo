bd143dcaec3b7844cf84109e2bb4c8988913fd5d
modiat <modiat@gmail.com>
2018-11-15 15:22:50 +0800

    調整 common/Http及common/Https, HTTP_DATA_TYPE為application/json的處理 調整 JSONUtils，增加API回應字串，結果為成功的判斷

--- app/src/main/java/org/iii/wheelpiedemo/course/util/JSONUtils.java
+++ app/src/main/java/org/iii/wheelpiedemo/course/util/JSONUtils.java
@@ -15,4 +15,17 @@ public class JSONUtils {
         }
         return jsonString;
     }
+
+    static public boolean isAPIResultSuccess(String apiResponse) {
+        boolean result = false;
+        if (apiResponse != null && apiResponse.length() != 0) {
+            try {
+                JSONObject jsonResp = new JSONObject(apiResponse);
+                result = jsonResp.optBoolean("result");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
 }
